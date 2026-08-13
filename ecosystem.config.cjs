07e00bcb5afe3cd10500c6b11c7131694f91f638
ic7y <ic7y@github.com>
2026-08-13 19:04:26 +0800

    配置修改

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -8,7 +8,9 @@ module.exports = {
       exec_mode: 'cluster',
       env: {
         NODE_ENV: 'production',     
-	      PORT: 8080
+	      PORT: 8080,
+        WX_APPID: 'wx890e079c68fa47bf',
+        WX_SECRET: 'c1e1f81643564b2cd0d45ac494ef03c2'
       }
     }
   ]
