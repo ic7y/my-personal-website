b511fa419a1ba8580219e1ea7f525c0ad9707dab
Ubuntu <ubuntu@localhost.localdomain>
2026-08-13 18:42:05 +0800

    修改ecosystem配置为fork模式并调整相关文件

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -5,7 +5,8 @@ module.exports = {
       name: 'my-nuxt-site', // ← 这就是名字来源
       script: './.output/server/index.mjs',
       instances: 'max',
-      exec_mode: 'cluster',
+      exec_mode: "fork",      // 改成fork，关闭集群
+      instances: 1,           // 只启动1个进程，列表只会显示一条
       env: {
         NODE_ENV: 'production',     
 	      PORT: 8080
